a411cfa540435976ea22d592d7ae27f65c31eb5b
José Alberto <daisuketsu1@gmail.com>
2016-03-24 17:36:42 -0500

    forms order

--- emusa12/emballages/forms.py
+++ emusa12/emballages/forms.py
@@ -34,7 +34,7 @@ class add_form1(forms.ModelForm):
 	        	self.fields['text_secundario111'].initial = index.objects.last().text_secundario111
 	        	self.fields['titulo_121'].widget.attrs['value'] = index.objects.last().titulo_121
 	        	self.fields['text_secundario121'].initial = index.objects.last().text_secundario121
-	        	# self.fields['titulo_131'].widget.attrs['value'] = index.objects.last().titulo_131
+	        	#self.fields['titulo_131'].widget.attrs['value'] = index.objects.last().titulo_131
 	        	self.fields['subtitulo131'].widget.attrs['value'] = index.objects.last().subtitulo131
 	        	self.fields['text_secundario131'].initial = index.objects.last().text_secundario131
 	        	self.fields['subtitulo132'].widget.attrs['value'] = index.objects.last().subtitulo132
@@ -268,6 +268,9 @@ class add_form2(forms.ModelForm):
 	        	self.fields['titulo_201'].widget.attrs['value'] = emballage.objects.last().titulo_201
 	        	self.fields['text_secundario201'].initial = emballage.objects.last().text_secundario201
 	        	self.fields['boton_201'].widget.attrs['value'] = emballage.objects.last().boton_201
+    			self.fields['boton_251'].widget.attrs['value'] = emballage.objects.last().boton_251
+		    	self.fields['boton_252'].widget.attrs['value'] = emballage.objects.last().boton_252
+	    		self.fields['boton_253'].widget.attrs['value'] = emballage.objects.last().boton_253
 			self.fields['titulo_211'].widget.attrs['value'] = emballage.objects.last().titulo_211
 			self.fields['titulo_212'].widget.attrs['value'] = emballage.objects.last().titulo_212
 			self.fields['titulo_213'].widget.attrs['value'] = emballage.objects.last().titulo_213
@@ -283,43 +286,40 @@ class add_form2(forms.ModelForm):
 			self.fields['empaque_229'].widget.attrs['value'] = emballage.objects.last().empaque_229
 			self.fields['titulo_251'].widget.attrs['value'] = emballage.objects.last().titulo_251
 			self.fields['cuestionario'].widget.attrs['value'] = emballage.objects.last().cuestionario
-        	self.fields['boton_251'].widget.attrs['value'] = emballage.objects.last().boton_251
-        	self.fields['boton_252'].widget.attrs['value'] = emballage.objects.last().boton_252
-        	self.fields['boton_253'].widget.attrs['value'] = emballage.objects.last().boton_253
-		self.fields['subtitulo2501'].initial = emballage.objects.last().subtitulo2501					        	
-		self.fields['subtitulo2502'].initial = emballage.objects.last().subtitulo2502					        	
-		self.fields['subtitulo2503'].initial = emballage.objects.last().subtitulo2503					        	
-		self.fields['subtitulo2504'].initial = emballage.objects.last().subtitulo2504					        	
-		self.fields['subtitulo2505'].initial = emballage.objects.last().subtitulo2505					        	
-		self.fields['subtitulo2506'].initial = emballage.objects.last().subtitulo2506					        	
-		self.fields['subtitulo2507'].initial = emballage.objects.last().subtitulo2507					        	
-		self.fields['subtitulo2508'].initial = emballage.objects.last().subtitulo2508					        	
-		self.fields['subtitulo2509'].initial = emballage.objects.last().subtitulo2509					        	
-		self.fields['subtitulo2510'].initial = emballage.objects.last().subtitulo2510					        	
-		self.fields['subtitulo2511'].initial = emballage.objects.last().subtitulo2511					        	
-		self.fields['subtitulo2512'].initial = emballage.objects.last().subtitulo2512					        	
-		self.fields['subtitulo2513'].initial = emballage.objects.last().subtitulo2513					        	
-		self.fields['subtitulo2514'].initial = emballage.objects.last().subtitulo2514					        	
-		self.fields['subtitulo2515'].initial = emballage.objects.last().subtitulo2515
-		self.fields['text_secundario201'].initial = emballage.objects.last().text_secundario201									  
-		self.fields['text_secundario211'].initial = emballage.objects.last().text_secundario211									  
-		self.fields['text_secundario212'].initial = emballage.objects.last().text_secundario212									  
-		self.fields['text_secundario2501'].initial = emballage.objects.last().text_secundario2501									  
-		self.fields['text_secundario2502'].initial = emballage.objects.last().text_secundario2502									  
-		self.fields['text_secundario2503'].initial = emballage.objects.last().text_secundario2503									  
-		self.fields['text_secundario2504'].initial = emballage.objects.last().text_secundario2504									  
-		self.fields['text_secundario2505'].initial = emballage.objects.last().text_secundario2505									  
-		self.fields['text_secundario2506'].initial = emballage.objects.last().text_secundario2506									  
-		self.fields['text_secundario2507'].initial = emballage.objects.last().text_secundario2507									  
-		self.fields['text_secundario2508'].initial = emballage.objects.last().text_secundario2508									  
-		self.fields['text_secundario2509'].initial = emballage.objects.last().text_secundario2509									  
-		self.fields['text_secundario2510'].initial = emballage.objects.last().text_secundario2510									  
-		self.fields['text_secundario2511'].initial = emballage.objects.last().text_secundario2511									  
-		self.fields['text_secundario2512'].initial = emballage.objects.last().text_secundario2512									  
-		self.fields['text_secundario2513'].initial = emballage.objects.last().text_secundario2513									  
-		self.fields['text_secundario2514'].initial = emballage.objects.last().text_secundario2514									  
-		self.fields['text_secundario2515'].initial = emballage.objects.last().text_secundario2515
-				
+			self.fields['subtitulo2501'].initial = emballage.objects.last().subtitulo2501					        	
+			self.fields['subtitulo2502'].initial = emballage.objects.last().subtitulo2502					        	
+			self.fields['subtitulo2503'].initial = emballage.objects.last().subtitulo2503					        	
+			self.fields['subtitulo2504'].initial = emballage.objects.last().subtitulo2504					        	
+			self.fields['subtitulo2505'].initial = emballage.objects.last().subtitulo2505					        	
+			self.fields['subtitulo2506'].initial = emballage.objects.last().subtitulo2506					        	
+			self.fields['subtitulo2507'].initial = emballage.objects.last().subtitulo2507					        	
+			self.fields['subtitulo2508'].initial = emballage.objects.last().subtitulo2508					        	
+			self.fields['subtitulo2509'].initial = emballage.objects.last().subtitulo2509					        	
+			self.fields['subtitulo2510'].initial = emballage.objects.last().subtitulo2510					        	
+			self.fields['subtitulo2511'].initial = emballage.objects.last().subtitulo2511					        	
+			self.fields['subtitulo2512'].initial = emballage.objects.last().subtitulo2512					        	
+			self.fields['subtitulo2513'].initial = emballage.objects.last().subtitulo2513					        	
+			self.fields['subtitulo2514'].initial = emballage.objects.last().subtitulo2514					        	
+			self.fields['subtitulo2515'].initial = emballage.objects.last().subtitulo2515
+			self.fields['text_secundario201'].initial = emballage.objects.last().text_secundario201									  
+			self.fields['text_secundario211'].initial = emballage.objects.last().text_secundario211									  
+			self.fields['text_secundario212'].initial = emballage.objects.last().text_secundario212									  
+			self.fields['text_secundario2501'].initial = emballage.objects.last().text_secundario2501									  
+			self.fields['text_secundario2502'].initial = emballage.objects.last().text_secundario2502									  
+			self.fields['text_secundario2503'].initial = emballage.objects.last().text_secundario2503									  
+			self.fields['text_secundario2504'].initial = emballage.objects.last().text_secundario2504									  
+			self.fields['text_secundario2505'].initial = emballage.objects.last().text_secundario2505									  
+			self.fields['text_secundario2506'].initial = emballage.objects.last().text_secundario2506									  
+			self.fields['text_secundario2507'].initial = emballage.objects.last().text_secundario2507									  
+			self.fields['text_secundario2508'].initial = emballage.objects.last().text_secundario2508									  
+			self.fields['text_secundario2509'].initial = emballage.objects.last().text_secundario2509									  
+			self.fields['text_secundario2510'].initial = emballage.objects.last().text_secundario2510									  
+			self.fields['text_secundario2511'].initial = emballage.objects.last().text_secundario2511									  
+			self.fields['text_secundario2512'].initial = emballage.objects.last().text_secundario2512									  
+			self.fields['text_secundario2513'].initial = emballage.objects.last().text_secundario2513									  
+			self.fields['text_secundario2514'].initial = emballage.objects.last().text_secundario2514									  
+			self.fields['text_secundario2515'].initial = emballage.objects.last().text_secundario2515
+					
 	class Meta:
 		model = emballage
 		fields = '__all__'
@@ -802,21 +802,22 @@ class add_form2(forms.ModelForm):
 class add_form3(forms.ModelForm):
 	def __init__(self, *args, **kwargs):           
 	    	super(add_form3, self).__init__(*args, **kwargs)
-	        self.fields['titulo_301'].widget.attrs['value'] = tecnology.objects.last().titulo_301
-        	self.fields['text_secundario301'].initial = tecnology.objects.last().text_secundario301
-        	self.fields['boton_301'].widget.attrs['value'] = tecnology.objects.last().boton_301
-        	self.fields['subtitulo311'].initial = tecnology.objects.last().subtitulo311
-        	self.fields['text_secundario311'].initial = tecnology.objects.last().text_secundario311
-        	self.fields['text_secundario312'].initial = tecnology.objects.last().text_secundario312
-        	self.fields['text_secundario313'].initial = tecnology.objects.last().text_secundario313
-        	self.fields['text_secundario314'].initial = tecnology.objects.last().text_secundario314
-        	self.fields['text_secundario315'].initial = tecnology.objects.last().text_secundario315
-        	self.fields['text_secundario316'].initial = tecnology.objects.last().text_secundario316
-        	self.fields['text_secundario317'].initial = tecnology.objects.last().text_secundario317
-        	self.fields['text_secundario318'].initial = tecnology.objects.last().text_secundario318
-        	self.fields['text_secundario319'].initial = tecnology.objects.last().text_secundario319
-        	self.fields['titulo_321'].widget.attrs['value'] = tecnology.objects.last().titulo_321
-        	self.fields['text_secundario321'].initial = tecnology.objects.last().text_secundario321
+	        if tecnology.objects.last():
+		        self.fields['titulo_301'].widget.attrs['value'] = tecnology.objects.last().titulo_301
+	        	self.fields['text_secundario301'].initial = tecnology.objects.last().text_secundario301
+	        	self.fields['boton_301'].widget.attrs['value'] = tecnology.objects.last().boton_301
+	        	self.fields['subtitulo311'].initial = tecnology.objects.last().subtitulo311
+	        	self.fields['text_secundario311'].initial = tecnology.objects.last().text_secundario311
+	        	self.fields['text_secundario312'].initial = tecnology.objects.last().text_secundario312
+	        	self.fields['text_secundario313'].initial = tecnology.objects.last().text_secundario313
+	        	self.fields['text_secundario314'].initial = tecnology.objects.last().text_secundario314
+	        	self.fields['text_secundario315'].initial = tecnology.objects.last().text_secundario315
+	        	self.fields['text_secundario316'].initial = tecnology.objects.last().text_secundario316
+	        	self.fields['text_secundario317'].initial = tecnology.objects.last().text_secundario317
+	        	self.fields['text_secundario318'].initial = tecnology.objects.last().text_secundario318
+	        	self.fields['text_secundario319'].initial = tecnology.objects.last().text_secundario319
+	        	self.fields['titulo_321'].widget.attrs['value'] = tecnology.objects.last().titulo_321
+	        	self.fields['text_secundario321'].initial = tecnology.objects.last().text_secundario321
 
 
 
@@ -929,12 +930,13 @@ class add_form4(forms.ModelForm):
 class add_form5(forms.ModelForm):
 	def __init__(self, *args, **kwargs):           
 	    	super(add_form5, self).__init__(*args, **kwargs)
-	        self.fields['titulo_501'].widget.attrs['value'] = contact.objects.last().titulo_501
-	        self.fields['pregunta51'].widget.attrs['placeholder'] = "Ingresa tu nombre"
-	        self.fields['pregunta52'].widget.attrs['placeholder'] = "Ingresa tu correo"
-	        self.fields['pregunta53'].widget.attrs['placeholder'] = "Ingresa tu número"
-	        self.fields['pregunta54'].widget.attrs['placeholder'] = "Escribe el mensaje"
-	        self.fields['boton_501'].widget.attrs['value'] = contact.objects.last().boton_501
+	        if contact.objects.last():
+		        self.fields['titulo_501'].widget.attrs['value'] = contact.objects.last().titulo_501
+		        self.fields['pregunta51'].widget.attrs['placeholder'] = "Ingresa tu nombre"
+		        self.fields['pregunta52'].widget.attrs['placeholder'] = "Ingresa tu correo"
+		        self.fields['pregunta53'].widget.attrs['placeholder'] = "Ingresa tu número"
+		        self.fields['pregunta54'].widget.attrs['placeholder'] = "Escribe el mensaje"
+		        self.fields['boton_501'].widget.attrs['value'] = contact.objects.last().boton_501
 
     
 	class Meta:
@@ -954,7 +956,8 @@ class add_form5(forms.ModelForm):
 class add_form6(forms.ModelForm):
 	def __init__(self, *args, **kwargs):           
 	    	super(add_form6, self).__init__(*args, **kwargs)
-	        self.fields['titulo_601'].widget.attrs['value'] = footer.objects.last().titulo_601
+	        if footer.objects.last():
+	        	self.fields['titulo_601'].widget.attrs['value'] = footer.objects.last().titulo_601
 	class Meta:
 		model = footer
 		fields = '__all__'
